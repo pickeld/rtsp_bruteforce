bc2d808c62fcf52691397ff640edbd15490877d4
pickeld <davidpickel>
2023-01-20 21:15:42 +0200

    init

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from config import Config
-from utils import PasswordGenerator, RTSPBruteForce
+from rtsp_brute_force import  RTSPBruteForce
+from password_generator import PasswordGenerator
 
 if __name__ == '__main__':
     configs = Config()
-    # if configs.generate_paswords:
-    #     pass_gen = PasswordGenerator()
-    #     pass_gen.password_gen_orch()
+    if configs.generate_paswords:
+        pass_gen = PasswordGenerator()
+        pass_gen.password_gen_orch()
     if configs.brute_force:
         brute_forcer = RTSPBruteForce()
         brute_forcer.run()
--- rtsp_brute_force.py
+++ rtsp_brute_force.py
@@ -1,55 +1,16 @@
 import base64
 import concurrent.futures
-import itertools
+
 import os
 import re
 import socket
 
 from concurrent.futures import ThreadPoolExecutor
-from multiprocessing import Process
-from pathlib import Path
+
 
 from config import Config
 
 
-class PasswordGenerator:
-    def __init__(self):
-        self.config = Config()
-        self.characters = self.config.characters
-        self.digits = self.config.digits
-        self.symbols = self.config.symbols
-        self.length = self.config.length
-
-    def password_gen_orch(self):
-        threads = []
-        for i in range(self.config.length['start'], self.config.length['end'] + 1):
-            t = Process(target=self.worker, args=(i,))
-            t.start()
-
-    def worker(self, i, config=Config()):
-        bulker = []
-        index = 0
-        print(f"Start {i}")
-        for item in itertools.product(config.characters + config.digits + config.symbols, repeat=i):
-            password = "".join(item)
-            print(password)
-            # if password == '28d@':
-            #     return True
-            if len(bulker) >= config.bulker_len:
-                self.save_passwords_file(bulker, config, i, index)
-                index += 1
-                bulker = []
-            bulker.append(password)
-        self.save_passwords_file(bulker, config, i, index)
-        print(f"Done {i}")
-
-    def save_passwords_file(self, bulker, config, i, index):
-        print(f'passwords_{i}_{index}.txt')
-        output_file = Path(os.path.join(config.passwords_dir, f'passwords_{i}_{index}.txt'))
-        output_file.parent.mkdir(exist_ok=True, parents=True)
-        with open(output_file, 'a') as f:
-            for s in bulker:
-                f.write(f'{s}\n')
 
 
 class RTSPBruteForce:
