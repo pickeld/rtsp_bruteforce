d8836fc8cdbcda6cce1df6294cb056b0b974e30d
pickeld <davidpickel>
2023-01-20 14:33:58 +0200

    refactoring

--- main.py
+++ main.py
@@ -16,28 +16,6 @@ class RTSPBruteForce:
         self.symbols = self.config.symbols
         self.length = self.config.length
 
-    def generate_password_dict(self):
-        bulker = []
-        for i in range(self.config.length['start'], self.config.length['end'] + 1):
-            for item in itertools.product(self.config.characters + self.config.digits + self.config.symbols, repeat=i):
-                password = "".join(item)
-                print(password)
-                if password == '0547755011':
-                    return True
-                # if len(bulker) >= self.config.bulker_len:
-                #     self.save_passwords_file(bulker)
-                #     bulker = []
-                # bulker.append(item)
-
-    def save_passwords_file(self, bulker):
-        return
-
-        file_path = os.path.join(self.config.filepath, self.config.filename)
-        with open(file_path, 'w') as f:
-            for i in bulker:
-                print(i)
-                # f.write(str(i))
-
     def get_response(self, response):
         if '200 OK' in str(response):
             return '200 OK'
